f62dcb816e0988af7ef8d4615ee1b5b1fe827de3
SergeyP11 <145446062+SergeyP11@users.noreply.github.com>
2023-12-10 18:34:10 +0300

    Add files via upload

---
+++ lab1/5/ConsoleApp1/Program.cs
@@ -0,0 +1,112 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the line:");
+        string inputstring = Console.ReadLine();
+
+        Console.WriteLine("Select an action:");
+        Console.WriteLine("a) Sort words alphabetically and output the last characters");
+        Console.WriteLine("b) Raise the case of the first letter and lower the case of the last letter in each word(the minimum word length is 2 letters)");
+        Console.WriteLine("c) Count the number of occurrences of a given word");
+        Console.WriteLine("d) Replace the penultimate word");
+        Console.WriteLine("e) Find the k-th word starting with a capital letter");
+
+        char choice = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        switch (choice)
+        {
+            case 'a':
+                SortAndPrintLastCharacters(inputstring);
+                break;
+            case 'b':
+                ModifyCase(inputstring);
+                break;
+            case 'c':
+                CountOccurrences(inputstring);
+                break;
+            case 'd':
+                ReplaceSecondToLastWord(inputstring);
+                break;
+            case 'e':
+                FindKthCapitalizedWord(inputstring);
+                break;
+            default:
+                Console.WriteLine("Wrong choice.");
+                break;
+        }
+    }
+
+    static void SortAndPrintLastCharacters(string input)
+    {
+        string[] words = input.Split(' ');
+        Array.Sort(words);
+        string result = new string(words.Select(w => w[w.Length - 1]).ToArray());
+        Console.WriteLine(result);
+    }
+
+    static void ModifyCase(string input)
+    {
+        string[] words = input.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            string word = words[i];
+            if (word.Length > 0)
+            {
+                char firstChar = char.ToUpper(word[0]);
+                char lastChar = char.ToLower(word[word.Length - 1]);
+                words[i] = firstChar + word.Substring(1, word.Length - 2) + lastChar;
+            }
+        }
+        string result = string.Join(" ", words);
+        Console.WriteLine(result);
+    }
+
+    static void CountOccurrences(string input)
+    {
+        Console.WriteLine("Enter the word to count:");
+        string wordToCount = Console.ReadLine();
+        int count = input.Split(' ').Count(w => w.Equals(wordToCount, StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine($"Word {wordToCount} occurs {count} time.");
+    }
+
+    static void ReplaceSecondToLastWord(string input)
+    {
+        Console.WriteLine("Enter a new word:");
+        string newWord = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+        if (words.Length >= 2)
+        {
+            words[words.Length - 2] = newWord;
+            string result = string.Join(" ", words);
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine("The string does not contain enough words to replace.");
+        }}
+
+    static void FindKthCapitalizedWord(string input)
+    {
+        Console.WriteLine("Enter the number k:");
+        int k = int.Parse(Console.ReadLine());
+
+        string[] words = input.Split(' ');
+        int count = 0;
+        foreach (string word in words)
+        {
+            if (char.IsUpper(word[0]))
+            {
+                count++;
+                if (count == k)
+                {
+                    Console.WriteLine($"k a word starting with a capital letter: {word}");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine("There is no single word starting with a capital letter in the line.");
+    }
+}
---
+++ lab1/6/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿public delegate double EquationDelegate(double x);
+
+public class DichotomyMethod
+{
+    public static double FindRoot(double a, double b, EquationDelegate equation, double precision)
+    {
+        if (equation(a) * equation(b) >= 0)
+        {
+            throw new ArgumentException("The function values at the interval endpoints must have different signs");
+        }
+
+        while ((b - a) >= precision)
+        {
+            double mid = (a + b) / 2;
+            if (equation(mid) == 0.0)
+            {
+                return mid;
+            }
+            else if (equation(mid) * equation(a) < 0)
+            {
+                b = mid;
+            }
+            else
+            {
+                a = mid;
+            }
+        }
+
+        return (a + b) / 2;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Пример x^2 - 4 = 0 на интервале от 1 до 3 с точностью 0.001
+        EquationDelegate equation1 = x => x * x - 4;
+        double root1 = DichotomyMethod.FindRoot(1, 3, equation1, 0.001);
+        Console.WriteLine("Root of the equation x^2 - 4 = 0 on the interval [1, 3] with precision 0.001: " + root1);
+
+        // Пример sin(x) = 0 на интервале от 1 до 4 с точностью 0.001
+        EquationDelegate equation2 = Math.Sin;
+        double root2 = DichotomyMethod.FindRoot(1, 4, equation2, 0.001);
+        Console.WriteLine("Root of the equation sin(x) = 0 on the interval [1, 4] with precision 0.0001: " + root2);
+    }
+}
